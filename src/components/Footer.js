ac4ea9d86d0ab3d2a517f8c508f4db7be920759e
repeatdomiau <gabriel.militello@gmail.com>
2020-11-09 20:09:44 -0300

    Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p>LojaDev - 2020 @ Todos os diretos reservados </p>
+        </div>
+    )
+}
+
+export default Footer;
